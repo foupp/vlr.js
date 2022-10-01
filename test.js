a67f11727f580d75755c27460687961d0c593d25
foupp <viggonarvaez27@hotmail.com>
2022-10-01 15:48:07 -0400

    update: testing

--- test.js
+++ test.js
@@ -5,30 +5,34 @@ import _, {
     getMatches,
     getPlayer,
     getPlayers,
-    getRankings
+    getRankings,
+    getTeam
 } from './index.mjs';
 
-_('/team/2').then(() => console.log('getPage - working')).catch(() => {
-    throw new Error('getPage - error');
+_('/team/2').then(() => console.log('getPage — working')).catch(() => {
+    throw new Error('getPage — error');
 }); /* Team */
-getEvent(1089).then(() => console.log('getEvent - working')).catch(() => {
-    throw new Error('getEvent - error');
+getEvent(1089).then(() => console.log('getEvent — working')).catch(() => {
+    throw new Error('getEvent — error');
 }); /* Event */
-getEvents().then(() => console.log('getEvents - working')).catch(() => {
-    throw new Error('getEvents - error');
+getEvents().then(() => console.log('getEvents — working')).catch(() => {
+    throw new Error('getEvents — error');
 }); /* Events */
-getMatchResults().then(() => console.log('getMatchResults - working')).catch(() => {
-    throw new Error('getMatchResults - error');
+getMatchResults().then(() => console.log('getMatchResults — working')).catch(() => {
+    throw new Error('getMatchResults — error');
 });  /* Match Results */
-getMatches().then(() => console.log('getMatches - working')).catch(() => {
-    throw new Error('getMatches - error');
+getMatches().then(() => console.log('getMatches — working')).catch(() => {
+    throw new Error('getMatches — error');
 });  /* Matches */
-getPlayer(9).then(() => console.log('getPlayer - working')).catch(() => {
-    throw new Error('getPlayer - error');
+getPlayer(9).then(() => console.log('getPlayer — working')).catch(() => {
+    throw new Error('getPlayer — error');
 });  /* Player */
-getPlayers().then(() => console.log('getPlayers - working')).catch(() => {
-    throw new Error('getPlayers - error');
+getPlayers().then(() => console.log('getPlayers — working')).catch(() => {
+    throw new Error('getPlayers — error');
 });  /* Players */
-getRankings().then(() => console.log('getRankings - working')).catch(() => {
-    throw new Error('getRankings - error');
+getRankings().then(() => console.log('getRankings — working')).catch(() => {
+    throw new Error('getRankings — error');
 });  /* Rankings */
+getTeam(2).then(() => console.log('getTeam — working')).catch(() => {
+    throw new Error('getTeam — error')
+})
